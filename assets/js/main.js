a7ec445e2baeaa7dcc10d919f281153e880ce139
Christian Söderberg <christian.v.soderberg@gmail.com>
2016-11-04 16:32:10 +0100

    Correct Week Function

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,10 @@ var months = ['Januari','Februari','Mars','April','Maj','Juni','Juli','Augusti',
 
 // Functions
 Date.prototype.getWeekNumber = function(){
-    var d = new Date(+this);
-    d.setHours(0,0,0);
-    d.setDate(d.getDate()+4-(d.getDay()||7));
-    return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+  var d = new Date(+this);
+  d.setHours(0,0,0,0);
+  d.setDate(d.getDate()+4-(d.getDay()||7));
+  return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
 };
 function stripPriceUnit(string){
   if (string === undefined){
